f58a96ca13b1dbbb8554530f3fbf78dee5aa000f
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2022-12-08 16:37:33 +0100

    Add project files.

---
+++ scripts/sample1.ts
@@ -0,0 +1,8 @@
+﻿let mapluis: google.maps.Map;
+
+function initMap1(): void {
+    mapluis = new google.maps.Map(document.getElementById("map1") as HTMLElement, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
---
+++ wwwroot/js/sample1.js
@@ -0,0 +1,8 @@
+var mapluis;
+function initMap1() {
+    mapluis = new google.maps.Map(document.getElementById("map1"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
+//# sourceMappingURL=sample1.js.map
